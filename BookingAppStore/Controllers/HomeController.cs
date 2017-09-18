5db56cc0c4734a68e82afe10c272c69d09ac23f0
Gareev Albert <alfaalbert1994@gmail.com>
2017-09-18 06:22:13 +0300

    Добавьте файлы проекта.

---
+++ BookingAppStore/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookingAppStore.Models;
+
+namespace BookingAppStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            var books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        
+        [HttpPost]
+        public string Buy(Purchase puchase)
+        {
+            puchase.Date = DateTime.Now;
+            db.Purchases.Add(puchase);
+            db.SaveChanges();
+            return "Thanks you, " + puchase.Person + ", for your purchase!";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BookingAppStore/Models/BookContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookingAppStore.Models
+{
+    public class BookContext: DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+    }
+
+    public class BookDbInitializer: DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            context.Books.Add(new Book { Name = "Книга 1", Author = "Автор 1", Price = 220});
+            context.Books.Add(new Book { Name = "Книга 2", Author = "Автор 2", Price = 160});
+            context.Books.Add(new Book { Name = "Книга 3", Author = "Автор 3", Price = 100});
+
+            base.Seed(context);
+        }
+    }
+}
